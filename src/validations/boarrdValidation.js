75ce837734cd7948889b1913de59eee5f01b7825
ghostoninternet <daosyphuc288@gmail.com>
2023-11-30 22:31:06 +0700

    Validate FE-sent data in BE with Joi

---
+++ src/validations/boarrdValidation.js
@@ -0,0 +1,35 @@
+import Joi from 'joi'
+import { StatusCodes } from 'http-status-codes'
+
+const createNew = async (req, res, next) => {
+  /**
+   * Mặc định chúng ta không cần phải custom message phía BE làm gì vì để cho FE 
+   * tự validate và custom message cho đẹp
+   * BE chỉ cần validate đảm bảo dữ liệu chuẩn xác, và trả về message mặc định phía thư viện là được
+   * Quan trọng: Việc validate ở BE là bắt buộc vì đây là điểm cuối để lưu trữ dữ liệu vào Database
+   * Và thông thường trong thực tế, việc tốt nhất là validate cả ở BE và FE
+   */
+  const correctCondition = Joi.object({
+    title: Joi.string().required().min(3).max(50).trim().strict().messages({
+      'any.required': 'Title is required',
+      'string.empty': 'Title is not allowed to be empty',
+      'string.min': 'Title min 3 chars',
+      'string.max': 'Title max 50 chars',
+      'string.trim': 'Title must not have leading or trailing whitespace'
+    }),
+    description: Joi.string().required().min(3).max(256).trim().strict()
+  })
+
+  try {
+    await correctCondition.validateAsync(req.body, { abortEarly: false })
+    res.status(StatusCodes.CREATED).json({ message: 'In board validation' })
+  } catch (error) {
+    res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
+      errors: new Error(error).message
+    })
+  }
+}
+
+export const boardValidation = {
+  createNew
+}
