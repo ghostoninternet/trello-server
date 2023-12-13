e686f841e02f4c222b1cfc901972ac8a7448f8bd
ghostoninternet <daosyphuc288@gmail.com>
2023-12-13 19:53:36 +0700

    Update cors not allow localhost

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 // Domains that are allowed to request back-end resource
 export const WHITELIST_DOMAINS = [
-  'http://localhost:5173'
+  // 'http://localhost:5173'
 ]
 
 export const BOARD_TYPES = {
