26ecad883f8e0c1539e8c1645ef61849fd6f87a5
ghostoninternet <daosyphuc288@gmail.com>
2023-12-13 22:12:24 +0700

    Config whitelist domains

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,8 @@
 // Domains that are allowed to request back-end resource
 export const WHITELIST_DOMAINS = [
   // 'http://localhost:5173'
+  'https://trello-web-orcin.vercel.app/',
+  'https://trello-web-phuc-daos-projects.vercel.app/'
 ]
 
 export const BOARD_TYPES = {
